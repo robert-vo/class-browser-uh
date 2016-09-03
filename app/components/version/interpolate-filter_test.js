3c4e6cb8baebb3e28dbd6b812e077a62095972a8
robert-vo <robertvo79@gmail.com>
2016-09-03 00:58:20 -0500

    Changed name of app from myApp to classBrowserUHApp.

--- app/components/version/interpolate-filter_test.js
+++ app/components/version/interpolate-filter_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('classBrowserUHApp.version module', function() {
+  beforeEach(module('classBrowserUHApp.version'));
 
   describe('interpolate filter', function() {
     beforeEach(module(function($provide) {
--- app/components/version/version_test.js
+++ app/components/version/version_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('classBrowserUHApp.version module', function() {
+  beforeEach(module('classBrowserUHApp.version'));
 
   describe('version service', function() {
     it('should return current version', inject(function(version) {
