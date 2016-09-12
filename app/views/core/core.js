1694d1d8cde139c0e7484cc05554fb906bd0d4b0
robert-vo <robertvo79@gmail.com>
2016-09-12 15:50:35 -0500

    Added functionality of finding core classes. Added population of dropdown box options through variables.

--- app/views/core/core.js
+++ app/views/core/core.js
@@ -8,7 +8,43 @@ angular.module('classBrowserUHApp.core', ['ngRoute'])
         controller: 'CoreCtrl'
     });
 }])
+.controller('CoreCtrl', function($scope, $http) {
 
-.controller('CoreCtrl', [function() {
+    $scope.resultSet = [];
+    $scope.numberOfRows = 0;
+    $scope.message = "";
 
-}]);
+    $scope.coreCategories = {
+        availableOptions: [
+            {categoryNumber: 1, categoryName: "I. Communication"},
+            {categoryNumber: 2, categoryName : "II.	Mathematics"},
+            {categoryNumber: 3, categoryName : "III.	Institutionally Designated Option: Math/Reasoning"},
+            {categoryNumber: 4, categoryName : "IV.	American History"},
+            {categoryNumber: 5, categoryName : "V.	Government"},
+            {categoryNumber: 6, categoryName : "VI.	Humanities"},
+            {categoryNumber: 7, categoryName : "VII.	Visual & Performing Arts"},
+            {categoryNumber: 8, categoryName : "VIII.	Natural Sciences"},
+            {categoryNumber: 9, categoryName : "IX.	Social & Behavioral Sciences"},
+            {categoryNumber: 10, categoryName : "X.	Writing in the Disciplines"}]
+    };
+
+    $scope.populateCoreClasses = function() {
+        console.log("User selected category number: " + $scope.coreCategory.categoryNumber);
+        console.log("Populating Core Classes...");
+
+        var url = $scope.apiUrl + '/core=' + $scope.coreCategory.categoryNumber;
+        $http.get(url)
+            .success(function (data) {
+                var message = "Retrieved " + data.numberOfRows + " core classes.";
+                console.log(message);
+                console.log(data);
+                $scope.resultSet = data.result;
+                $scope.showDiv = true;
+                $scope.numberOfRows = data.numberOfRows;
+                $scope.message = message;
+            })
+            .error(function (data) {
+                alert("Unable to retrieve the data.");
+            });
+    }
+    });
