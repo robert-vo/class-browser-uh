c661fb316fab9e3898ef6afeaa05bb3296f089c9
robert-vo <robertvo79@gmail.com>
2016-09-11 21:27:28 -0500

    Added angular-environment configuration.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('classBrowserUHApp', [
     'ngRoute',
     'classBrowserUHApp.version',
@@ -9,10 +8,41 @@ angular.module('classBrowserUHApp', [
     'classBrowserUHApp.core',
     'classBrowserUHApp.department',
     'classBrowserUHApp.offeredclass',
-    'classBrowserUHApp.navbar'
+    'classBrowserUHApp.navbar',
+    'environment'
 ]).
+config(function(envServiceProvider) {
+    // set the domains and variables for each environment
+    envServiceProvider.config({
+        domains: {
+            development: ['localhost', 'dev.local'],
+            production: ['acme.com', 'acme.net', 'acme.org']
+            // anotherStage: []
+        },
+        vars: {
+            development: {
+                apiUrl: '//localhost/api',
+                staticUrl: '//localhost/static'
+                // antoherCustomVar: ''
+            },
+            production: {
+                apiUrl: '//api.acme.com/v2',
+                staticUrl: '//static.acme.com'
+                // antoherCustomVar: ''
+            }
+        }
+    });
+
+    // run the environment check, so the comprobation is made
+    // before controllers and services are built
+    envServiceProvider.check();
+}).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
     $routeProvider.otherwise({redirectTo: '/home'});
+}]).
+controller('EnvVarCtrl', ['$scope', 'envService', function($scope, envService) {
+    $scope.environment = envService.get(); // store the current environment
+    $scope.vars = envService.read('all');
 }]);
