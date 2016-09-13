509d80cbd5036e78dfc64468c9179c00e695795a
robert-vo <robertvo79@gmail.com>
2016-09-12 19:41:51 -0500

    Edited core categories and added a second message to be printed on the view.

--- app/views/core/core.js
+++ app/views/core/core.js
@@ -13,19 +13,23 @@ angular.module('classBrowserUHApp.core', ['ngRoute'])
     $scope.resultSet = [];
     $scope.numberOfRows = 0;
     $scope.message = "";
+    $scope.messageTwo = "";
 
     $scope.coreCategories = {
         availableOptions: [
-            {categoryNumber: 1, categoryName: "I. Communication"},
-            {categoryNumber: 2, categoryName : "II.	Mathematics"},
-            {categoryNumber: 3, categoryName : "III.	Institutionally Designated Option: Math/Reasoning"},
-            {categoryNumber: 4, categoryName : "IV.	American History"},
-            {categoryNumber: 5, categoryName : "V.	Government"},
-            {categoryNumber: 6, categoryName : "VI.	Humanities"},
-            {categoryNumber: 7, categoryName : "VII.	Visual & Performing Arts"},
-            {categoryNumber: 8, categoryName : "VIII.	Natural Sciences"},
-            {categoryNumber: 9, categoryName : "IX.	Social & Behavioral Sciences"},
-            {categoryNumber: 10, categoryName : "X.	Writing in the Disciplines"}]
+            {categoryNumber: 1, categoryName: "Communication"},
+            {categoryNumber: 2, categoryName : "Mathematics"},
+            {categoryNumber: 3, categoryName : "Life and Physical Sciences"},
+            {categoryNumber: 4, categoryName : "Language, Philosophy & Culture"},
+            {categoryNumber: 5, categoryName : "Creative Arts"},
+            {categoryNumber: 6, categoryName : "American History"},
+            {categoryNumber: 7, categoryName : "Government/Political Science"},
+            {categoryNumber: 8, categoryName : "Social & Behavioral Sciences"},
+            {categoryNumber: 9, categoryName : "Government/Political Science"},
+            {categoryNumber: 10, categoryName : "Writing in the Disciplines"}],
+        selectedOption: {
+                categoryNumber: 1, categoryName: "I. Communication"
+        }
     };
 
     $scope.populateCoreClasses = function() {
@@ -35,13 +39,17 @@ angular.module('classBrowserUHApp.core', ['ngRoute'])
         var url = $scope.apiUrl + '/core=' + $scope.coreCategory.categoryNumber;
         $http.get(url)
             .success(function (data) {
-                var message = "Retrieved " + data.numberOfRows + " core classes.";
+                var message = "Retrieved " + data.numberOfRows +
+                    " classes that fall under the " + data.result[0].core_title + " category.";
+                var messageTwo = "The " + data.result[0].core_title + " core category requires " +
+                    data.result[0].hours_required + " hours to be taken to fulfill the core requirement.";
                 console.log(message);
                 console.log(data);
                 $scope.resultSet = data.result;
                 $scope.showDiv = true;
                 $scope.numberOfRows = data.numberOfRows;
                 $scope.message = message;
+                $scope.messageTwo = messageTwo;
             })
             .error(function (data) {
                 alert("Unable to retrieve the data.");
