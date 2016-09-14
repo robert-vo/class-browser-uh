16577313703d414872bd904fab38f4fb00dd40e6
robert-vo <robertvo79@gmail.com>
2016-09-14 17:09:00 -0500

    Added a function to generate messages displayed to the user. Added a function to clear the forms.

--- app/views/class/class.js
+++ app/views/class/class.js
@@ -65,8 +65,9 @@ controller('ClassCtrl', ['$scope', '$http', function ($scope, $http) {
         $http
             .get(apiUrl)
             .success(function (data) {
-                $scope.message = "You selected whatever";
-                $scope.messageTwo = "Retrieved " + data.numberOfRows + " classes.";
+                $scope.subjectMessage = generateMessage($scope.departmentModel, 'subjects', 'departmentFullName');
+                $scope.creditHoursMessage = generateMessage($scope.creditHourModel, 'credit hour', 'creditHours');
+                $scope.coreMessage = generateMessage($scope.coreModel, 'core classes', 'categoryName');
                 $scope.rowCollection = [];
                 $scope.rowCollection = data.result;
                 $scope.showDiv = true;
@@ -79,28 +80,43 @@ controller('ClassCtrl', ['$scope', '$http', function ($scope, $http) {
             });
     };
 
-    $scope.populateClasses = function(){
+    var generateMessage = function(model, type, objToPluck) {
+        if(model == undefined || model.length == 0) {
+            return "You have selected zero " + type + ". Searching for all " + type + ".";
+        }
+        else {
+            return "You have selected " + _.pluck(model, objToPluck).join(', ') + " as your " + type + ".";
+        }
+    };
 
+
+    $scope.populateClasses = function(){
         var apiUrl = $scope.apiUrl + '/information?department=aas&credit_hours=2';
 
         // http://localhost:8080/api/information?department=aas&credit_hours=2
         // department - A String that represents the department class(es) belong to.
         // credit-hours - An integer that represents the number of credit hours a class fulfills.
         // core - A String representing the core categories of a class.
-
-        // console.log("User selected department: " + $scope.department);
-        // console.log("User selected credit-hours: " + $scope.creditHours);
-        // console.log("User selected core: " + $scope.core);
+        console.log("User selected department: " + $scope.departmentModel);
+        console.log("User selected credit-hours: " + $scope.creditHourModel);
+        console.log("User selected core: " + $scope.coreModel);
         console.log("Populating Classes...");
-        // console.log($scope.departmentModel);
-        // console.log($scope.creditHourModel);
-        // console.log($scope.coreModel);
         runPopulateClasses(apiUrl);
     };
 
     $scope.goBack = function() {
         console.log('going back');
         $scope.showDiv = false;
-    }
+    };
+
+    $scope.clearForms = function() {
+        console.log('clearing forms...');
+        console.log($scope.departmentModel);
+        console.log($scope.creditHourModel);
+        console.log($scope.coreModel);
+        $scope.departmentModel = undefined;
+        $scope.creditHourModel = undefined;
+        $scope.coreModel = undefined;
+    };
 
 }]);
