20137205a6704f4ff757818d9619893eecbb93e2
robert-vo <robertvo79@gmail.com>
2016-09-18 00:00:19 -0500

    Initial commit - Added terms 1980 to 2070.

--- app/views/offeredclass/offeredclass.js
+++ app/views/offeredclass/offeredclass.js
@@ -9,6 +9,38 @@ angular.module('classBrowserUHApp.offeredclass', ['ngRoute'])
     });
 }])
 
-.controller('OfferedClassCtrl', [function() {
+.controller('OfferedClassCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.format = [
+        'Online',
+        'Face to Face',
+        'Hybrid'
+    ];
+
+    $scope.options = [
+        'Alpha',
+        'Bravo',
+        'Charlie',
+        'Delta',
+        'Echo',
+        'Fox',
+        'Golf'
+    ];
+
+    //validate
+    $scope.terms = {
+        availableOptions: [
+            {
+                termID: 1970,
+                year: 2015,
+                semester: "Fall"
+            },
+            {
+                termID: 1980,
+                year: 2016,
+                semester: "Springeruhgfd"
+            }
+        ]
+    }
+
 
 }]);
