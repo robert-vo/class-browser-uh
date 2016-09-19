3a08336acd0923c94f3643f4bb4c2125474b32ef
robert-vo <robertvo79@gmail.com>
2016-09-19 16:40:15 -0500

    Added options for format.

--- app/views/offeredclass/offeredclass.js
+++ app/views/offeredclass/offeredclass.js
@@ -10,11 +10,17 @@ angular.module('classBrowserUHApp.offeredclass', ['ngRoute'])
 }])
 
 .controller('OfferedClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope, $http, $q, $rootScope) {
-    $scope.format = [
-        'Online',
-        'Face to Face',
-        'Hybrid'
-    ];
+
+    $scope.format = {
+        availableOptions: [
+        {formatType: 'Online', isOrNot: 'Is'},
+        {formatType: 'Not Online', isOrNot: 'Not'},
+        {formatType: 'Not Face to Face', isOrNot: 'Not'},
+        {formatType: 'Face to Face', isOrNot: 'Is'},
+        {formatType: 'Not Hybrid', isOrNot: 'Not'},
+        {formatType: 'Hybrid', isOrNot: 'Is'}
+    ]
+    };
 
     $rootScope.httpService.getData('resources/terms.json').then(function(result) {
         $scope.terms = result;
