5c9c2e79263c76a75b827bb53d6b0fe8510d0f97
robert-vo <robertvo79@gmail.com>
2016-09-19 17:10:56 -0500

    Set format options from JSON file, resources/format.json.

--- app/views/offeredclass/offeredclass.js
+++ app/views/offeredclass/offeredclass.js
@@ -11,16 +11,9 @@ angular.module('classBrowserUHApp.offeredclass', ['ngRoute'])
 
 .controller('OfferedClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope, $http, $q, $rootScope) {
 
-    $scope.format = {
-        availableOptions: [
-        {formatType: 'Online', isOrNot: 'Is'},
-        {formatType: 'Not Online', isOrNot: 'Not'},
-        {formatType: 'Not Face to Face', isOrNot: 'Not'},
-        {formatType: 'Face to Face', isOrNot: 'Is'},
-        {formatType: 'Not Hybrid', isOrNot: 'Not'},
-        {formatType: 'Hybrid', isOrNot: 'Is'}
-    ]
-    };
+    $rootScope.httpService.getData('resources/format.json').then(function(result) {
+        $scope.format = result;
+    });
 
     $rootScope.httpService.getData('resources/terms.json').then(function(result) {
         $scope.terms = result;
