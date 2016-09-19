3059f2f3aa44a4c26434532adbb49ce54f353648
robert-vo <robertvo79@gmail.com>
2016-09-19 09:28:36 -0500

    Moved Array.prototype.allParametersUndefinedOrNull and isArrayIsUndefinedOrNull to app.js under arrayService. Refactored setting of departments/creditHours/core options. Refactored retrieval of api URL data.

--- app/views/class/class.js
+++ app/views/class/class.js
@@ -11,36 +11,17 @@ controller('ClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope
     $scope.selectedCountLimit = 'count > 3';
     $scope.rowCollection = [];
 
-    $scope.departments = {
-        availableOptions: []
-    };
+    $rootScope.httpService.getData('resources/subjects.json').then(function(result) {
+        $scope.departments = result;
+    });
 
-    $scope.departments = $http.get('resources/subjects.json')
-        .success(function (data) {
-            return $scope.departments = data;
-        })
-        .error(function (data) {
-            alert("Unable to find subjects.json.");
-        });
+    $rootScope.httpService.getData('resources/creditHours.json').then(function(result) {
+        $scope.creditHours = result;
+    });
 
-
-    $scope.creditHours =
-        $http.get('resources/creditHours.json')
-            .success(function (data) {
-                $scope.creditHours = data;
-            })
-            .error(function (data) {
-                alert("Unable to find creditHours.json.");
-            });
-
-    $scope.coreCategories =
-        $http.get('resources/coreCategories.json')
-            .success(function (data) {
-                $scope.coreCategories = data;
-            })
-            .error(function (data) {
-                alert("Unable to find coreCategories.json.");
-            });
+    $rootScope.httpService.getData('resources/coreCategories.json').then(function(result) {
+        $scope.coreCategories = result;
+    });
 
     var generateMessage = function(model, type, objToPluck) {
         if(model == undefined || model.length == 0) {
@@ -60,16 +41,11 @@ controller('ClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope
         $scope.numberOfRows = 0;
         for(var i = 0; i < apiUrl.length; i++) {
             console.log("Got URL: " + apiUrl[i]);
-            $http
-                .get(apiUrl[i])
-                .success(function (data) {
-                    $scope.numberOfRows += data.numberOfRows;
-                    $scope.rowCollection = $scope.rowCollection.concat(data.result);
-                    populateFields();
-                })
-                .error(function (data) {
-                    alert("Unable to retrieve the data.");
-                });
+            $rootScope.httpService.getData(apiUrl[i]).then(function(result) {
+                $scope.numberOfRows += result.numberOfRows;
+                $scope.rowCollection = $scope.rowCollection.concat(result.result);
+                populateFields();
+            });
         }
     };
 
@@ -100,18 +76,6 @@ controller('ClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope
         }
     };
 
-    var isArrayIsUndefinedOrNull = function(arr) {
-        return !(_.isUndefined(arr) || _.isNull(arr) || _.isEmpty(arr));
-    };
-
-    Array.prototype.allParametersUndefinedOrNull = function() {
-        for(var i = 0; i < this.length; i++) {
-            if(isArrayIsUndefinedOrNull(this[i])) {
-                return false;
-            }
-        }
-        return true;
-    };
 
     var buildApiUrlsFromModel = function(department, creditHour, core) {
         console.log("Building API URL...");
@@ -128,19 +92,15 @@ controller('ClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope
         creditHour = _.pluck(creditHour, 'creditHours');
         core = _.pluck(core, 'categoryNumber');
 
-        console.log("dept " + department);
-        console.log("crh " + creditHour);
-        console.log("core " + core);
 
-
-        if(isArrayIsUndefinedOrNull(department)) {
-            $rootScope.apiURLService.expandArrayValues(department, 'department');
+        if($rootScope.arrayService.isArrayIsUndefinedOrNull(department)) {
+            $rootScope.apiURLService.expandArrayValuesInPlace(department, 'department');
         }
-        if(isArrayIsUndefinedOrNull(creditHour)) {
-            $rootScope.apiURLService.expandArrayValues(creditHour, 'credit-hours');
+        if($rootScope.arrayService.isArrayIsUndefinedOrNull(creditHour)) {
+            $rootScope.apiURLService.expandArrayValuesInPlace(creditHour, 'credit-hours');
         }
-        if(isArrayIsUndefinedOrNull(core)) {
-            $rootScope.apiURLService.expandArrayValues(core, 'core');
+        if($rootScope.arrayService.isArrayIsUndefinedOrNull(core)) {
+            $roresultotScope.apiURLService.expandArrayValuesInPlace(core, 'core');
         }
 
         var allParameters = [department,creditHour,core]
