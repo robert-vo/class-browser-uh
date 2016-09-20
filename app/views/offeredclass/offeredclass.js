f2057308e7e588ea6beafc476d6cad927241090c
robert-vo <robertvo79@gmail.com>
2016-09-19 23:26:43 -0500

    Refactored how available options for dropdown lists are set.

--- app/views/offeredclass/offeredclass.js
+++ app/views/offeredclass/offeredclass.js
@@ -8,55 +8,68 @@ angular.module('classBrowserUHApp.offeredclass', ['ngRoute'])
         controller: 'OfferedClassCtrl'
     });
 }])
-.controller('OfferedClassCtrl', ['$scope', '$http', '$q', '$rootScope', function ($scope, $http, $q, $rootScope) {
+.controller('OfferedClassCtrl', ['$scope', '$http', '$q', '$rootScope', '$parse', function ($scope, $http, $q, $rootScope, $parse) {
+    var allJSONAndScopeNames = [
+        {
+            "path": "resources/format.json",
+            "scopeName": "format"
+        },
+        {
+            "path": "resources/terms.json",
+            "scopeName": "terms"
+        },
+        {
+            "path": "resources/status.json",
+            "scopeName": "status"
+        },
+        {
+            "path": "resources/session.json",
+            "scopeName": "session"
+        },
+        {
+            "path": "resources/subjects.json",
+            "scopeName": "departments"
+        },
+        {
+            "path": "resources/buildings.json",
+            "scopeName": "buildings"
+        },
+        {
+            "path": "resources/locations.json",
+            "scopeName": "locations"
+        },
+        {
+            "path": "resources/component.json",
+            "scopeName": "component"
+        },
+        {
+            "path": "resources/creditHours.json",
+            "scopeName": "creditHours"
+        },
+        {
+            "path": "resources/weekendU.json",
+            "scopeName": "weekendU"
+        },
+        {
+            "path": "resources/coreCategories.json",
+            "scopeName": "coreCategories"
+        }
+    ];
+
     var deleteModel = function() {
         [].slice.call(arguments).forEach(function(arg) {
             delete $scope[arg];
         });
     };
 
-    $rootScope.httpService.getData('resources/format.json').then(function(result) {
-        $scope.format = result;
-    });
+    var setScopeVariableFromJSON = function(filePath, scopeVariable) {
+        $rootScope.httpService.getData(filePath).then(function(result) {
+            $parse(scopeVariable).assign($scope, result);
+        });
+    };
 
-    $rootScope.httpService.getData('resources/terms.json').then(function(result) {
-        $scope.terms = result;
-    });
-
-    $rootScope.httpService.getData('resources/status.json').then(function(result) {
-        $scope.status = result;
-    });
-
-    $rootScope.httpService.getData('resources/session.json').then(function(result) {
-        $scope.session = result;
-    });
-
-    $rootScope.httpService.getData('resources/subjects.json').then(function(result) {
-        $scope.departments = result;
-    });
-
-    $rootScope.httpService.getData('resources/buildings.json').then(function(result) {
-        $scope.buildings = result;
-    });
-
-    $rootScope.httpService.getData('resources/locations.json').then(function(result) {
-        $scope.locations = result;
-    });
-
-    $rootScope.httpService.getData('resources/component.json').then(function(result) {
-        $scope.component = result;
-    });
-
-    $rootScope.httpService.getData('resources/creditHours.json').then(function(result) {
-        $scope.creditHours = result;
-    });
-
-    $rootScope.httpService.getData('resources/coreCategories.json').then(function(result) {
-        $scope.coreCategories = result;
-    });
-
-    $rootScope.httpService.getData('resources/weekendU.json').then(function(result) {
-        $scope.weekendU = result;
+    allJSONAndScopeNames.forEach(function(e) {
+       setScopeVariableFromJSON(e.path, e.scopeName);
     });
 
     $scope.clearForms = function() {
