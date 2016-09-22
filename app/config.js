d22c4abc21c7fc79a61a476558b39275d666cced
robert-vo <robertvo79@gmail.com>
2016-09-22 15:22:12 -0500

    Initial commit - Content from app.js.

---
+++ app/config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('classBrowserUHApp').
+config(function(nyaBsConfigProvider) {
+    var nyaSearchBoxMessages = {
+        defaultNoneSelection: 'Nothing Selected',
+        noSearchResult: 'No Search Results Found!',
+        numberItemSelected: '%d Items Selected'
+    };
+    nyaBsConfigProvider.setLocalizedText('en-us', nyaSearchBoxMessages);
+}).
+config(function(envServiceProvider) {
+    envServiceProvider.config({
+        domains: {
+            development : ['localhost'],
+            production: ['classbrowseruh.firebaseapp']
+        },
+        vars: {
+            development: {
+                apiUrl: '//localhost:8080/api'
+            },
+            production: {
+                apiUrl: 'http://classbrowseruh.us-west-2.elasticbeanstalk.com/api'
+            }
+        }
+    });
+    envServiceProvider.check();
+}).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider.otherwise({redirectTo: '/home'});
+}]);
