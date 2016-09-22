503765b048f5718695a9733dfbd8821839961c2c
robert-vo <robertvo79@gmail.com>
2016-09-22 15:21:51 -0500

    Moved app config to config.js.

--- app/app.js
+++ app/app.js
@@ -14,41 +14,6 @@ angular.module('classBrowserUHApp', [
     'nya.bootstrap.select',
     'ngSanitize'
 ]).
-config(function(nyaBsConfigProvider) {
-    var nyaSearchBoxMessages = {
-        defaultNoneSelection: 'Nothing Selected',
-        noSearchResult: 'No Search Results Found!',
-        numberItemSelected: '%d Items Selected'
-    };
-    nyaBsConfigProvider.setLocalizedText('en-us', nyaSearchBoxMessages);
-}).
-config(function(envServiceProvider) {
-    envServiceProvider.config({
-        domains: {
-            development : ['localhost'],
-            production: ['https://classbrowseruh.firebaseapp.com/#!/home',
-                    'classbrowseruh.firebaseapp',
-                    'firebaseapp',
-                    'https://classbrowseruh.firebaseapp.com/#!/',
-                    'classbrowseruh.firebaseapp.com',
-                    'classbrowseruh.firebaseapp.com/',
-                    'classbrowseruh.firebaseapp.com/#!/']
-        },
-        vars: {
-            development: {
-                apiUrl: '//localhost:8080/api'
-            },
-            production: {
-                apiUrl: 'http://classbrowseruh.us-west-2.elasticbeanstalk.com/api'
-            }
-        }
-    });
-    envServiceProvider.check();
-}).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider.otherwise({redirectTo: '/home'});
-}]).
 controller('EnvVarCtrl', ['$scope', 'envService', function($scope, envService) {
     $scope.environment = envService.get();
     $scope.vars = envService.read('all');
