aad090b07ecff3c9fc1ca77820371c5d3dd10fd2
robert-vo <robertvo79@gmail.com>
2016-09-22 15:06:29 -0500

    Directory Change

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,13 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    basePath: 'app/app',
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/components/**/*.js',
       'view*/**/*.js'
     ],
 
