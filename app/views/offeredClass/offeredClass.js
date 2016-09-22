755757b944d6a85d3239cdac8023a05929af0cf5
robert-vo <robertvo79@gmail.com>
2016-09-22 16:44:16 -0500

    allJSONAndScopeNames now loads from a JSON file.

--- app/views/offeredClass/offeredClass.js
+++ app/views/offeredClass/offeredClass.js
@@ -9,97 +9,22 @@ angular.module('classBrowserUHApp.offeredClass', ['ngRoute'])
     });
 }])
 .controller('OfferedClassCtrl', ['$scope', '$http', '$q', '$rootScope', '$parse', '$window', function ($scope, $http, $q, $rootScope, $parse, $window) {
-    var allJSONAndScopeNames = [
-        {
-            "path": "resources/terms.json",
-            "scopeName": "terms",
-            "modelName": "termModel",
-            "parameter": "term"
-        },
-        {
-            "path": "resources/format.json",
-            "scopeName": "format",
-            "modelName": "formatModel",
-            "parameter": "format"
-        },
-        {
-            "path": "resources/status.json",
-            "scopeName": "status",
-            "modelName": "statusModel",
-            "parameter": "status"
-        },
-        {
-            "path": "resources/session.json",
-            "scopeName": "session",
-            "modelName": "sessionModel",
-            "parameter": "session"
-        },
-        {
-            "path": "resources/subjects.json",
-            "scopeName": "departments",
-            "modelName": "departmentModel"
-        },
-        {
-            "path": "resources/buildings.json",
-            "scopeName": "buildings",
-            "modelName": "buildingModel"
-        },
-        {
-            "path": "resources/locations.json",
-            "scopeName": "locations",
-            "modelName": "locationModel"
-        },
-        {
-            "path": "resources/component.json",
-            "scopeName": "component",
-            "modelName": "componentModel"
-        },
-        {
-            "path": "resources/creditHours.json",
-            "scopeName": "creditHours",
-            "modelName": "creditHourModel"
-        },
-        {
-            "path": "resources/weekendU.json",
-            "scopeName": "weekendU",
-            "modelName": "weekendUModel"
-        },
-        {
-            "path": "resources/coreCategories.json",
-            "scopeName": "coreCategories",
-            "modelName": "coreCategoriesModel"
-        },
-        {
-            "path": "resources/isCore.json",
-            "scopeName": "isCore",
-            "modelName": "isCoreModel"
-        },
-        {
-            "path": "resources/syllabus.json",
-            "scopeName": "syllabus",
-            "modelName": "syllabusModel"
-        },
-        {
-            "path": "resources/classDays.json",
-            "scopeName": "classDays",
-            "modelName": "classDaysModel"
-        },
-        {
-            "path": "",
-            "scopeName": "courseNumber",
-            "modelName": "courseNumberModel"
-        }
-    ];
-
     var setScopeVariableFromJSON = function(filePath, scopeVariable) {
         $rootScope.httpService.getData(filePath).then(function(result) {
             $parse(scopeVariable).assign($scope, result);
         });
     };
 
-    allJSONAndScopeNames.forEach(function(e) {
-        setScopeVariableFromJSON(e.path, e.scopeName);
-    });
+    var initializeAllJSONAndScopeNames = function() {
+        $rootScope.httpService.getData('resources/allScopeVariables.json').then(function(result) {
+            $scope.allJSONAndScopeNames = result.offeredClass;
+            $scope.allJSONAndScopeNames.forEach(function(e) {
+                setScopeVariableFromJSON(e.path, e.scopeName);
+            });
+        });
+    };
+
+    initializeAllJSONAndScopeNames();
 
     $scope.validValues = ['a', 'A','1','2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
@@ -113,7 +38,7 @@ angular.module('classBrowserUHApp.offeredClass', ['ngRoute'])
         var clearFormDialogConfirmation = $window.confirm('Are you sure you want to clear the entire form?');
 
         if (clearFormDialogConfirmation) {
-            allJSONAndScopeNames.forEach(function(e) {
+            $scope.allJSONAndScopeNames.forEach(function(e) {
                 $scope.deleteModel(e.modelName);
             });
         }
@@ -147,7 +72,7 @@ angular.module('classBrowserUHApp.offeredClass', ['ngRoute'])
                 }
             };
 
-            allJSONAndScopeNames.forEach(function(e) {
+            $scope.allJSONAndScopeNames.forEach(function(e) {
                 var obj = $scope.$eval(e.modelName);
                 console.log(e.parameter + "=" + obj[e.parameter]);
             });
