3934483eb04ad763ebd29642154bd1fd0e88dbcd
robert-vo <robertvo79@gmail.com>
2016-09-25 00:32:44 -0500

    Re-indented code.

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('classBrowserUHApp.home', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'views/home/home.html',
+            controller: 'HomeCtrl'
+        });
+    }])
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'views/home/home.html',
-        controller: 'HomeCtrl'
-    });
-}])
+    .controller('HomeCtrl', [function () {
 
-.controller('HomeCtrl', [function() {
-
-}]);
+    }]);
