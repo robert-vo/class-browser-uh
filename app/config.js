46a0936989f82a2897f5fd691c52d56ef52b8438
robert-vo <robertvo79@gmail.com>
2016-09-25 01:14:55 -0500

    Added github pages to production urls.

--- app/config.js
+++ app/config.js
@@ -11,7 +11,7 @@ angular.module('classBrowserUHApp').config(function (nyaBsConfigProvider) {
     envServiceProvider.config({
         domains: {
             development: ['localhost'],
-            production: ['classbrowseruh.firebaseapp']
+            production: ['classbrowseruh.firebaseapp', 'robert-vo.github.io']
         },
         vars: {
             development: {
